dad597085f07f55ca40c75fc8bf85d2abea2244b
Spinaldash <blueavenger@gmail.com>
2015-02-16 20:34:07 -0800

    got FAQ up and running

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   {method: 'get', path: '/{param*}', config: require('../routes/general/static')},
-  {method: 'get', path: '/', config: require('../routes/general/home')}
+  {method: 'get', path: '/', config: require('../routes/general/home')},
+  {method: 'get', path: '/faq', config: require('../routes/general/faq')}
 ];
